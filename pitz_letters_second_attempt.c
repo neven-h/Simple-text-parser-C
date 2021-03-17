2fdda4415611cffdcf4e7cf228ab26946d63cd1e
neven-h <69196529+neven-h@users.noreply.github.com>
2021-03-17 13:06:46 +0200

    Add files via upload

---
+++ pitz_letters_second_attempt.c
@@ -0,0 +1,40 @@
+#include <stdio.h>
+#include <ctype.h>
+#define IN 1
+#define OUT 0
+int  main(){
+int newSentnce = 1; /*mode: a beginning of a new sentence*/
+int quotation = 0; /*mode: entering into a quote*/
+char c;
+
+printf("\nPlease enter a text:\n");
+
+c = getchar();
+
+while(c != EOF){
+if(isdigit(c) != OUT) {newSentnce = OUT;}
+else {
+if(isspace(c) == OUT)
+{
+if(newSentnce == IN || quotation == IN) {
+if (islower(c) != OUT) /* change from lower cases to upper */
+c = toupper(c);
+}
+else
+{
+if(isupper(c) != OUT)
+c = tolower(c);
+}
+newSentnce = OUT;
+}
+putchar(c);
+
+
+if (c == '"' && quotation == OUT) quotation = IN; /*if we entered inside a quoate*/
+if (c == '.' && quotation == OUT) newSentnce = IN; /*if we're at the beginning of a line*/
+else if (c == '"' && quotation == IN) quotation = OUT; /*if we're at the end of a quoate*/
+}
+c = getchar();
+}
+return 0;
+}
